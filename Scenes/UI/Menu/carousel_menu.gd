18d0f7fd74d58a80525d68971e4a88948578690b
Gabriel Smereka <gsmereka@c2r9p5.42sp.org.br>
2025-09-20 22:19:50 -0300

    update menu

---
+++ Scenes/UI/Menu/carousel_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var vetor: Array
+func _ready() -> void:
+	vetor.append(get_children())
+	
+@onready var current_node = $TextureRect
+@onready var next_node = $TextureRect2
+@onready var prev_node = $TextureRect3
+
+func _process(delta: float) -> void:
+	position.x -= 300 * delta
+	var temp_node
+	if current_node.global_position.x < 0:
+		prev_node.global_position.x = next_node.global_position.x + 1920
+		temp_node = prev_node
+		prev_node = current_node
+		current_node = next_node
+		next_node = temp_node
+	
+	pass
