e62bf16dec86aa5d4a626c82b0c3f83693947db8
Jonas Alberto <jonasper19@gmail.com>
2025-09-21 03:32:23 -0300

    ok

--- Scenes/Game/characters/Player/animated_sprite_2d.gd
+++ Scenes/Game/characters/Player/animated_sprite_2d.gd
@@ -1,14 +1,13 @@
 extends AnimatedSprite2D
 
 func _process(delta):
-		var life = get_parent().get_parent().life
+	var life = get_parent().get_parent().life
+	# Certifique-se de que a animação certa está selecionada
+	if animation != "default":
+		animation = "default"
+		stop()  # Parar a animação para definir o frame manualmente
 
-		# Certifique-se de que a animação certa está selecionada
-		if animation != "default":
-			animation = "default"
-			stop()  # Parar a animação para definir o frame manualmente
+	var total_frames = sprite_frames.get_frame_count("default")
+	var frame_index = clamp(life, 0, total_frames - 1)
 
-		var total_frames = sprite_frames.get_frame_count("default")
-		var frame_index = clamp(life, 0, total_frames - 1)
-
-		frame = frame_index
+	frame = frame_index
