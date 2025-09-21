f0576b17a0b37ae784f43c02894eba944535f5b6
gsmereka <gsmereka200@gmail.com>
2025-09-21 09:01:12 -0300

    update: boss bullets

--- Scenes/Game/characters/enemy/bullet.gd
+++ Scenes/Game/characters/enemy/bullet.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var moveSpeed: float = 300.0
+var moveSpeed: float = 200.0
 var direct = Vector2.ZERO
 
 func _ready() -> void:
@@ -10,3 +10,10 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	position += moveSpeed * delta * direct
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if body.has_method("take_damage"):
+			body.take_damage()
+	pass # Replace with function body.
