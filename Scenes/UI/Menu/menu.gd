1350f22ff127cdd92130d505224b4827c6021b8a
gsmereka <gsmereka200@gmail.com>
2025-09-21 04:36:41 -0300

    feature: implements intro

--- Scenes/UI/Menu/menu.gd
+++ Scenes/UI/Menu/menu.gd
@@ -1,14 +1,24 @@
 extends Control
 
 @export var vel : int = 300
+@export var main_scene : String = "res://Scenes/Game/Placeholder/Placeholder.tscn"
+var to_play = false
 
 func _on_start_pressed() -> void:
-	SceneLoader.load_scene("res://Scenes/Game/Placeholder/Placeholder.tscn")
+	if to_play:
+		return
+	$Camera2D/LabelPularIntro.show()
+	$AnimationPlayer.play("Intro")
+	to_play = true
 	pass # Replace with function body.
 
-
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Esc"):
+		SceneLoader.load_scene(main_scene)
 
 func _on_config_pressed() -> void:
+	if to_play:
+		return
 	var node = preload("res://Scenes/UI/Config/config.tscn")
 	var config = node.instantiate()
 	add_child(config)
@@ -16,10 +26,20 @@ func _on_config_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
+	if to_play:
+		return
 	SceneLoader.load_scene("res://Scenes/Credits/Credits.tscn")
 	pass # Replace with function body.
 
 
 func _on_exit_pressed() -> void:
+	if to_play:
+		return
 	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if to_play:
+		SceneLoader.load_scene(main_scene)
 	pass # Replace with function body.
