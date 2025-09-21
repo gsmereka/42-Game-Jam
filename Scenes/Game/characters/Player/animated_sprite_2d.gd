ba5122adadbba1c11119306e1931b89004bebcf3
Jonas Alberto <jonasper19@gmail.com>
2025-09-20 23:22:31 -0300

    ok

---
+++ Scenes/Game/characters/Player/animated_sprite_2d.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+func _process(delta):
+		var life = get_parent().get_parent().life
+
+		# Certifique-se de que a animação certa está selecionada
+		if animation != "default":
+			animation = "default"
+			stop()  # Parar a animação para definir o frame manualmente
+
+		var total_frames = sprite_frames.get_frame_count("default")
+		var frame_index = clamp(life, 0, total_frames - 1)
+
+		frame = frame_index
--- Scenes/Game/characters/Player/player.gd
+++ Scenes/Game/characters/Player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var moveSpeed: float = 200.0;
+@export var life: int = 5
 
 func _physics_process(delta: float) -> void:
 	pass
